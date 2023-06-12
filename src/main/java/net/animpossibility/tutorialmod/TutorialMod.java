3e45314c0bc72c699aa9a7f5d37e988975217472
AnImpossibility <105756699+AnImpossibility@users.noreply.github.com>
2023-06-12 17:59:29 -0400

    Initial commit

---
+++ src/main/java/net/animpossibility/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.animpossibility.tutorialmod;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
---
+++ src/main/java/net/animpossibility/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.animpossibility.tutorialmod;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
